202a8de32f21157b149553cf5a788e104703d7d2
Mahmoud Salhab <mahmoud@salhab.work>
2022-03-29 17:13:58 +0300

    ConvModule implemented

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import math
-from turtle import forward
 from typing import List
 import torch
 import torch.nn as nn
@@ -133,7 +132,7 @@ class MHSA(nn.Module):
         ]
 
     def forward(self, inp: Tensor) -> Tensor:
-        """Passes the input into multi-head attention 
+        """Passes the input into multi-head attention
 
         Args:
             inp (Tensor): The input tensor
@@ -155,9 +154,70 @@ class MHSA(nn.Module):
         return inp + out
 
 
-
 class ConvModule(nn.Module):
-    pass
+    """Implements the convolution module
+    where it contains the following layers
+    1. Layernorm
+    2. Pointwise Conv
+    3. Gate Linear unit
+    4. 1D Depthwise conv
+    5. BatchNorm
+    6. Swish Activation
+    7. Pointwise Conv
+    8. Dropout
+
+    Args:
+        enc_dim (int): The encoder dimensionality.
+        scaling_factor (int): The scaling factor of the conv layer.
+        kernel_size (int): The convolution kernel size.
+        p_dropout (float): The dropout probability.
+    """
+    def __init__(
+            self,
+            enc_dim: int,
+            scaling_factor: int,
+            kernel_size: int,
+            p_dropout: float
+            ) -> None:
+        super().__init__()
+        self.lnorm = nn.LayerNorm(enc_dim)
+        n_scaled_channels = enc_dim * scaling_factor
+        assert (kernel_size - 1) % 2 == 0, 'kernel_size - 1 \
+            must be divisable by 2 -odd'
+        padding_size = (kernel_size - 1) // 2
+        self.pwise_conv1 = nn.Conv1d(
+            in_channels=enc_dim,
+            out_channels=n_scaled_channels,
+            kernel_size=1
+        )
+        self.glu = nn.GLU(dim=1)
+        self.dwise_conv = nn.Conv1d(
+            in_channels=enc_dim,
+            out_channels=enc_dim,
+            kernel_size=kernel_size,
+            padding=padding_size,
+            groups=enc_dim
+        )
+        self.bnorm = nn.BatchNorm1d(enc_dim)
+        self.swish = nn.SiLU()
+        self.dropout = nn.Dropout(p_dropout)
+        self.pwise_conv2 = nn.Conv1d(
+            in_channels=enc_dim,
+            out_channels=enc_dim,
+            kernel_size=1
+        )
+
+    def forward(self, inp: Tensor) -> Tensor:
+        out = self.lnorm(inp)
+        out = out.permute(0, 2, 1)
+        out = self.pwise_conv1(out)
+        out = self.glu(out)
+        out = self.dwise_conv(out)
+        out = self.bnorm(out)
+        out = self.swish(out)
+        out = self.pwise_conv2(out)
+        out = self.dropout(out)
+        return out + inp
 
 
 class FeedForwardModule(nn.Module):
