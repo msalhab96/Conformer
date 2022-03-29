ea1072bd45a4e6167894d2afe388412bccc45abd
Mahmoud Salhab <mahmoud@salhab.work>
2022-03-29 15:00:11 +0300

    base functionality added to MHSA

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
+import math
+import torch
 import torch.nn as nn
 from torch import Tensor
+from functools import lru_cache
 
 
 class ConformerBlock(nn.Module):
@@ -7,7 +10,113 @@ class ConformerBlock(nn.Module):
 
 
 class MHSA(nn.Module):
-    pass
+    def __init__(
+            self,
+            enc_dim: int,
+            dk: int,
+            p_dropout: float,
+            device: str
+            ) -> None:
+        super().__init__()
+        assert enc_dim % dk == 0, 'enc_dim is not divisible by dk'
+        self.fc_key = nn.Linear(
+            in_features=enc_dim,
+            out_features=enc_dim,
+        )
+        self.fc_query = nn.Linear(
+            in_features=enc_dim,
+            out_features=enc_dim,
+        )
+        self.fc_value = nn.Linear(
+            in_features=enc_dim,
+            out_features=enc_dim,
+        )
+        self.lnorm = nn.LayerNorm(enc_dim)
+        self.dropout = nn.Dropout(p_dropout)
+        self.dk = dk
+        self.sqrt_dk = math.sqrt(dk)
+        self.h = enc_dim // dk
+        self.softmax = nn.Softmax(dim=-1)
+        self.device = device
+
+    def _key_query_matmul(self, Q: Tensor, K: Tensor) -> Tensor:
+        """Performs the Matmul operation in
+        scaled Dot-Product Attention
+
+        Args:
+            Q (Tensor): The Query tensor of shape [B, M, dk, h]
+            K (Tensor): The Key tensor of shape [B, M, dk, h]
+
+        Returns:
+            Tensor: The result of matmul operation of shape
+            [B, M, dk, dk]
+        """
+        return torch.matmul(Q, K.permute(0, 1, 3, 2))
+
+    def _get_scaled_att(
+            self,
+            Q: Tensor,
+            K: Tensor
+            ) -> Tensor:
+        """Calculates the scaled attention map
+        by calculating softmax(matmul(Q, K.T)/sqrt(dk))
+
+        Args:
+            Q (Tensor): The Query tensor of shape [B, M, dk, h]
+            K (Tensor): The Key tensor of shape [B, M, dk, h]
+
+        Returns:
+            Tensor: The scaled attention map of shape
+            [B, M, dk, dk]
+        """
+        result = self._key_query_matmul(Q, K)
+        result /= self.sqrt_dk
+        return self.softmax(result)
+
+    def perform_self_att(
+            self,
+            Q: Tensor,
+            K: Tensor,
+            V: Tensor
+            ) -> Tensor:
+        """Perform multi head scaled attention
+        by calculating softmax(matmul(Q, K.T)/sqrt(dk)).V
+
+        Args:
+            Q (Tensor): The Query tensor of shape [B, M, dk, h]
+            K (Tensor): The Key tensor of shape [B, M, dk, h]
+            V (Tensor): The Value tensor of shape [B, M, dk, h]
+
+        Returns:
+            Tensor: The scaled attention map of shape
+            [B, M, dk * h]
+        """
+        (b, m, *_) = Q.shape
+        att = self._get_scaled_att(Q, K)
+        result = torch.matmul(att, V)
+        return result.view(b, m, -1)
+
+    @lru_cache(maxsize=2)
+    def get_positionals(self, max_length: int, enc_dim: int) -> Tensor:
+        """Create Positionals tensor to be added to the input
+
+        Args:
+            max_length (int): The maximum length
+            enc_dim (int): the encoder model dimensionality
+
+        Returns:
+            Tensor: Positional tensor
+        """
+        result = torch.zeros(max_length, enc_dim)
+        pos = torch.arange(0, enc_dim).repeat(max_length, 1)
+        i = torch.arange(0, max_length).repeat(enc_dim, 1).T
+        result[:, 1::2] = torch.sin(
+            pos[:, 1::2] / (10000 ** ((2 * i[:, 1::2]) / self.dk))
+            )
+        result[:, 0::2] = torch.cos(
+            pos[:, 0::2] / (10000 ** ((2 * i[:, 0::2]) / self.dk))
+            )
+        return result
 
 
 class ConvModule(nn.Module):
