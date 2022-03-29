ea881f80c0ae46e0c22aafd54dfc8e8bf13405e5
Mahmoud Salhab <mahmoud@salhab.work>
2022-03-29 09:43:27 +0300

    the model's building blocks added to model.py

---
+++ model.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+
+class ConformerBlock(nn.Module):
+    pass
+
+
+class MHSA(nn.Module):
+    pass
+
+
+class ConvModule(nn.Module):
+    pass
+
+
+class FeedForwardModule(nn.Module):
+    pass
+
+
+class Model(nn.Module):
+    pass
