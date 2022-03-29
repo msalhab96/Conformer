d04d6ad1ce0703ed11ac5d1c2dbfa4492869c621
Mahmoud Salhab <mahmoud@salhab.work>
2022-03-29 10:52:59 +0300

    FeedForwardModule implemented

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from torch import Tensor
 
 
 class ConformerBlock(nn.Module):
@@ -14,7 +15,58 @@ class ConvModule(nn.Module):
 
 
 class FeedForwardModule(nn.Module):
-    pass
+    """Implements the feed forward module in the conformer block
+    where the module consists of the below
+    1. Layer Norm
+    2. Linear Layer
+    3. Swish Activation
+    4. Dropout
+    5. Linear Layer
+    6. Dropout
+
+    Args:
+        enc_dim (int): The encoder dimensionality
+        scaling_factor (int): The scaling factor of the linear layer
+        p_dropout (float): The dropout probability
+    """
+    def __init__(
+            self,
+            enc_dim: int,
+            scaling_factor: int,
+            p_dropout: float
+            ) -> None:
+        super().__init__()
+        scaled_dim = scaling_factor * enc_dim
+        self.lnorm = nn.LayerNorm(enc_dim)
+        self.fc1 = nn.Linear(
+            in_features=enc_dim,
+            out_features=scaled_dim
+        )
+        self.fc2 = nn.Linear(
+            in_features=scaled_dim,
+            out_features=enc_dim
+        )
+        self.swish = nn.SiLU()
+        self.dropout = nn.Dropout(p_dropout)
+
+    def forward(self, inp: Tensor) -> Tensor:
+        """Passes the given inp through the feed forward
+        module
+
+        Args:
+            inp (Tensor): the input to the feed forward module
+            with shape [B, M, N] where B is the batch size, M
+            is the maximum length, and N is the encoder dim
+        Returns:
+            Tensor: The result of the forward module
+        """
+        out = self.lnorm(inp)
+        out = self.fc1(out)
+        out = self.swish(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        out = self.dropout(out)
+        return inp + out
 
 
 class Model(nn.Module):
