fa413bfa0d809885fa7e7191d0af7eff8853e9d1
Mahmoud Salhab <mahmoud@salhab.work>
2022-04-01 20:34:49 +0300

    print statement deleted

--- train.py
+++ train.py
@@ -1,12 +1,24 @@
 import os
+from pathlib import Path
 import torch
-from hprams import hprams
+from hprams import (
+    get_audpipe_params,
+    get_model_params,
+    get_optim_params,
+    hprams
+    )
 from functools import wraps
 from torch.nn import Module
 from data import DataLoader
 from torch.optim import Optimizer
-from typing import Callable
+from typing import Callable, Union
 from tqdm import tqdm
+from model import Model
+
+from optim import AdamWarmup
+from pipelines import get_pipelines
+from tokenizer import CharTokenizer, ITokenizer
+from utils import IPipeline
 
 MODEL_KEY = 'model'
 OPTIMIZER_KEY = 'optimizer'
@@ -98,6 +110,9 @@ class Trainer:
             x = x.to(self.device)
             y = y.to(self.device)
             result = self.model(x)
+            result = result.log_softmax(axis=-1)
+            result = result.permute(1, 0, 2)
+            y = y[..., :result.shape[0]]
             input_lengths = torch.full(
                 size=(y.shape[0],),
                 fill_value=y.shape[1],
@@ -121,6 +136,7 @@ class Trainer:
         """Iterates over the whole training data and train the models
         for a single epoch
         """
+        torch.autograd.set_detect_anomaly(True)
         total_loss = 0
         self.set_train_mode()
         for x, (y, target_lengths) in tqdm(self.train_loader):
@@ -128,6 +144,9 @@ class Trainer:
             y = y.to(self.device)
             self.optimizer.zero_grad()
             result = self.model(x)
+            result = result.log_softmax(axis=-1)
+            result = result.permute(1, 0, 2)
+            y = y[..., :result.shape[0]]
             input_lengths = torch.full(
                 size=(y.shape[0],),
                 fill_value=y.shape[1],
@@ -147,3 +166,115 @@ class Trainer:
             self.history[self.__train_loss_key].append(total_loss)
         else:
             self.history[self.__train_loss_key] = [total_loss]
+
+
+def get_criterion(blank_id: int) -> Module:
+    return torch.nn.CTCLoss(blank_id)
+
+
+def get_optimizer(model: Module, params: dict) -> object:
+    return AdamWarmup(
+        model.parameters(),
+        **params
+        )
+
+
+def load_model(
+        model_params: dict,
+        checkpoint_path=None
+        ) -> Module:
+    model = Model(**model_params)
+    if checkpoint_path is not None:
+        model.load_state_dict(
+            torch.load(checkpoint_path)[MODEL_KEY]
+            )
+    return model
+
+
+def get_data_loader(
+        file_path: Union[str, Path],
+        tokenizer: ITokenizer,
+        text_pipeline: IPipeline,
+        audio_pipeline: IPipeline,
+        ):
+    return DataLoader(
+        file_path,
+        text_pipeline,
+        audio_pipeline,
+        tokenizer,
+        hprams.training.batch_size,
+        hprams.data.sampling_rate,
+        hprams.data.hop_length,
+        hprams.data.files_sep,
+        hprams.data.csv_file_keys,
+    )
+
+
+def get_tokenizer():
+    tokenizer = CharTokenizer()
+    if hprams.tokenizer.tokenizer_file is not None:
+        tokenizer = tokenizer.load_tokenizer(
+            hprams.tokenizer.tokenizer_file
+            )
+    tokenizer = tokenizer.add_blank_token().add_pad_token()
+    with open(hprams.tokenizer.vocab_path, 'r') as f:
+        vocab = f.read().split('\n')
+    tokenizer.set_tokenizer(vocab)
+    tokenizer.save_tokenizer('tokenizer.json')
+    return tokenizer
+
+
+def get_train_test_loaders(
+        tokenizer: ITokenizer,
+        audio_pipeline: IPipeline,
+        text_pipeline: IPipeline
+        ) -> tuple:
+    return (
+        get_data_loader(
+            hprams.data.training_file,
+            tokenizer,
+            text_pipeline,
+            audio_pipeline
+            ),
+        get_data_loader(
+            hprams.data.testing_file,
+            tokenizer,
+            text_pipeline,
+            audio_pipeline
+            )
+    )
+
+
+def get_trainer() -> Trainer:
+    device = hprams.device
+    tokenizer = get_tokenizer()
+    blank_id = tokenizer.special_tokens.blank_id
+    vocab_size = tokenizer.vocab_size
+    text_pipeline, audio_pipeline = get_pipelines(
+        get_audpipe_params()
+    )
+    model = load_model(
+        get_model_params(vocab_size),
+        checkpoint_path=hprams.checkpoint
+        ).to(device)
+    train_loader, test_loader = get_train_test_loaders(
+        tokenizer,
+        audio_pipeline,
+        text_pipeline
+    )
+    return Trainer(
+        criterion=get_criterion(blank_id),
+        optimizer=get_optimizer(
+            model, get_optim_params()
+            ),
+        model=model,
+        device=device,
+        train_loader=train_loader,
+        test_loader=test_loader,
+        epochs=hprams.training.epochs
+    )
+
+
+if __name__ == '__main__':
+    trainer = get_trainer()
+    trainer.fit()
